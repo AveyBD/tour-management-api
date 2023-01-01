6bea2e1203d29725208bdc8151e58a0b959df45e
Debashish Dev <hello@debashish.dev>
2023-01-01 18:36:53 +0600

    Filtering Logic

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -5,8 +5,45 @@ const {
 
 module.exports.getTours = async (req, res, next) => {
   let filters = { ...req.query };
-  const tours = await getTourService();
-  res.send(tours);
+  const excludeFields = ["sort", "page", "limit", "fields"]; // Excluding some field which will be used in other operation.
+
+  let filtersString = JSON.stringify(filters); // stringifying filters so that we can run replace operation
+  filtersString = filtersString.replace(
+    /\b(gt|gte|lt|lte|ne)\b/g,
+    (match) => `$${match}`
+  );
+  filters = JSON.parse(filtersString);
+  excludeFields.forEach((field) => delete filters[field]);
+  const queries = {};
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    queries.sortBy = sortBy;
+  }
+  if (req.query.fields) {
+    const fields = req.query.fields.split(",").join(" ");
+    // console.log(fields);
+    queries.fields = fields;
+  }
+
+  if (req.query.page) {
+    const { page = 1, limit = 10 } = req.query;
+    const skipValue = (page - 1) * (limit * 1);
+    queries.skip = skipValue;
+    queries.limit = parseInt(limit);
+  }
+  try {
+    const tours = await getTourService();
+    res.status(200).json({
+      success: true,
+      data: tours,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: "Can't get data",
+      error: error,
+    });
+  }
 };
 
 module.exports.postTours = async (req, res, next) => {
